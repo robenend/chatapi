a7b6f5fe0f6fd650178c71d3ba26493768f6663e
robenend <roberaendale2@gmail.com>
2024-08-13 22:32:30 +0300

    port 80

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 

92698c97e2810180eecebe231e63b42352493420
robenend <roberaendale2@gmail.com>
2024-08-13 21:46:50 +0300

    path

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ const io = new Server(expressServer, {
     origin:
       process.env.NODE_ENV === 'production'
         ? false
-        : ['http://localhost:5500', 'http://127.0.0.1:5500'],
+        : [
+            'https://chatapi-tan-ten.vercel.app/5500',
+            'https://chatapi-tan-ten.vercel.app/5500',
+          ],
   },
 });
 
